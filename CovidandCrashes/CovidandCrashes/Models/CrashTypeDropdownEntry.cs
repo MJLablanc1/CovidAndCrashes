35015dc3ea168f88902636d51f11db0ba87e3d19
Ian Beard <ianbeard0159@gmail.com>
2022-06-07 13:31:24 -0700

    Made some data models

---
+++ CovidandCrashes/CovidandCrashes/Models/CrashTypeDropdownEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace CovidandCrashes.Models
+{
+    public class CrashTypeDropdownEntry
+    {
+        // Main page uses a list of these to populate a dropdown list
+        public int crashTypeID { get; set; }
+        public int crashTypeName { get; set; }
+    }
+}
---
+++ CovidandCrashes/CovidandCrashes/Models/StateDropdownEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace CovidandCrashes.Models
+{
+    public class StateDropdownEntry
+    {
+        // Used to populate the state dropdown, main
+        //    page wil need a list of these
+
+        public int stateID { get; set; }
+        public string? stateName { get; set; }
+    }
+}
